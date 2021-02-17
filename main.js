a0ed0508ed9d72eff8097c05469fe37374164895
Rijin Mk <rijin.mk9@gmail.com>
2021-02-18 03:06:12 +0400

    DeviceOrientation

---
+++ main.js
@@ -0,0 +1,15 @@
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+function handleOrientation(event) {
+    var absolute = event.absolute;
+    var alpha    = event.alpha;
+    var beta     = event.beta;
+    var gamma    = event.gamma;
+
+    document.getElementsByClassName('absolute')[0].innerHTML = absolute;
+    document.getElementsByClassName('alpha')[0].innerHTML = alpha;
+    document.getElementsByClassName('beta')[0].innerHTML = beta;
+    document.getElementsByClassName('gamma')[0].innerHTML = gamma;
+  
+    // Do stuff with the new orientation data
+}
